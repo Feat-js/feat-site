e26c4a7ae708806728ffe93450b7c35700b2b041
serakoi <admin@seer-software.com>
2021-12-10 12:06:51 +0100

    Initial site

---
+++ comps/ui/hero_section.js
@@ -0,0 +1,55 @@
+module.exports = {
+    name: "HeroMain",
+    FeatAttributesSupport: true,
+    description: "Main hero with title etc.",
+    selfClosing: false,
+    attributes: [
+        {
+            name: "subtitle",
+            type: "string",
+            description: "Subtitle of the Hero",
+            required: false
+        },
+        {
+            name: "id",
+            type: "string",
+            description: "The id of the container",
+            required: false
+        }
+    ],
+    code: (attributes, content, data, registerComponent) => {
+        /*
+        Src: ""
+        */
+
+        let attr = [];
+        if (attributes.id) attr.push(`id="${attributes.id}"`);
+
+        return `
+            <div class="container mt-4" ${attr.join(" ")}>
+                <div class="text-center">
+
+                </div>
+
+                <h1>
+                    <img src="https://github.com/Feat-js/assets/blob/main/feat.js-icon_flat.png?raw=true" width="70" height="70"/>
+                    <strong>Feat</strong>.js
+                </h1>
+                <p>${attributes.subtitle ? attributes.subtitle : "The Feat.JS Framework; Easy, powerful and fast."}</p>
+
+                <a href="//github.com/Feat-JS/feat" class="mdc-button mdc-button--outlined">
+                    <span class="mdc-button__ripple"></span>
+                    <span class="mdc-button__label"><i class="fab fa-github"></i> Github</span>
+                </a>
+                <a href="/search" class="mdc-fab mdc-fab--extended fixed">
+                    <div class="mdc-fab__ripple"></div>
+                    <span class="material-icons mdc-fab__icon">search</span>
+                    <span class="mdc-fab__label">Search</span>
+                </a>
+                <hr/>
+                ${content}
+            </div>
+        `;
+
+    }
+} 
---
+++ index.js
@@ -0,0 +1,47 @@
+const Express = require('express');
+const app = new Express();
+const FeatJS = require('feat.js');
+const feat = new FeatJS(app);
+
+app.use(Express.static('public'));
+let pages = ["docs"];
+
+
+
+app.get('/', (req, res) => {
+    return res.render('/views/index', { 
+        inner: `views/inner/index.html`,
+        is_search: false
+    });
+});
+
+app.get('/search', (req, res) => {
+    let q = req.query.q;
+
+    let result = [];
+    if(!q) result = pages;
+
+    if(q) result = pages.filter(s => s.includes(q.toString()));
+
+    return res.render('/views/index', { 
+        inner: `views/inner/index.html`,
+        is_search: true,
+        pages: result,
+        query: q ? q : ""
+    });
+});
+
+app.get('/:id', (req, res) => {
+    if(!pages[req.params.id]) return res.render('/views/index', { 
+        inner: `views/inner/error.html`,
+        type: '404',
+        is_search: false
+    });
+
+    return res.render('/views/index', { 
+        inner: req.params.id,
+        is_search: false
+    });
+});
+
+app.listen(3000, () => console.log(`App online on port 3000`));
